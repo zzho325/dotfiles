a3c89a054fa6a898fdfaa5e56dd587da2ff364f1
Ashley Zhou <ashley.zhou@samsara.com>
2025-06-28 16:56:08 -0700

    change theme

--- nvim/.config/nvim/lua/ash/auto-dark-mode.lua
+++ nvim/.config/nvim/lua/ash/auto-dark-mode.lua
@@ -1,23 +1,23 @@
 require("rose-pine").setup({
-  variant     = "auto",    -- automatically choose based on background
-  dark_variant = "moon",   -- use “moon” when background = dark
+	variant      = "auto", -- automatically choose based on background
+	dark_variant = "main", -- use “moon” when background = dark
 })
 
 -- apply once for initial colorscheme
 vim.cmd("colorscheme rose-pine")
 
 require("auto-dark-mode").setup({
-  update_interval = 3000,
+	update_interval = 3000,
 
-  set_dark_mode = function()
-    vim.opt.background = "dark"
-    vim.cmd("colorscheme rose-pine")
-  end,
+	set_dark_mode = function()
+		vim.opt.background = "dark"
+		vim.cmd("colorscheme rose-pine")
+	end,
 
-  set_light_mode = function()
-    vim.opt.background = "light"
-    vim.cmd("colorscheme rose-pine")
-  end,
+	set_light_mode = function()
+		vim.opt.background = "light"
+		vim.cmd("colorscheme rose-pine")
+	end,
 
-  fallback = "dark",
+	fallback = "dark",
 })
